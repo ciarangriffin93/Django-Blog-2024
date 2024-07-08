d0c8de2469b3b00202e25dfa221f06ad06913d2f
Ciaran Griffin <griffinciaran3@gmail.com>
2024-07-08 10:21:35 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog2-ar6c49e092t.ws.codeinstitute-ide.net','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-ciarangriff-djangoblog2-fot3728utl8.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
